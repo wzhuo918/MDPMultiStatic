72b857c9e0a326fba959ed09565523ab38e50b1e
wzhuo918 <wzhuo918@gmail.com>
2015-06-17 10:07:38 +0800

    完成初步的MDP，没有决策时间点

--- src/MPDmain.java
+++ src/MPDmain.java
@@ -1,3 +1,11 @@
+/**
+ * 本程序是使用马尔科夫决策过程来解决多轮分区的问题
+ * 
+ * Description: (1) read data from a file
+ * 				(2) MDP
+ * 				(3) write output in a file 
+ **/
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
@@ -5,28 +13,41 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
 
-/**
- * 本程序是使用马尔科夫决策过程来解决多轮分区的问题
- **/
-
 public class MPDmain {
-	public int PNUM = 40;
+	public int PNUM = 100;
 	public Map<Integer, Integer> counter = new HashMap<Integer, Integer>();
 	//二维数组，第一行
 	public int[][] Sampletable = new int[3][PNUM];
 	public int[] ReduceLoad = new int[4];
 
-	public int TOTALNUM = 25000;
-	public int samplenum = 0;
+	public int TOTALNUM = 100000;
+	public int Samplenum = 0;
+	public boolean First = true;
+	public int AssPnum = 0;
+	public int unAssPnum = 0;
+	
+	public int UNAssiPNUM = -1;
 
 	public int State[] = new int[PNUM];
 	public int Action[] = new int[PNUM];
-	public float Problity = (float) 0.0;
 
-	//read write io
+	public double beta = 0.0;
+
+	public List<Integer> AssignedPar = new LinkedList<Integer>();
+	public List<Integer> unAssignedPar = new LinkedList<Integer>();
+
+	/**
+	 * Read and write a file
+	 * 
+	 * @param fileName
+	 * @throws IOException
+	 */
 	public void readFileByLines(String fileName) throws IOException {
 		File file = new File(fileName);
 		BufferedReader reader = null;
@@ -34,7 +55,7 @@ public class MPDmain {
 		//String tempString =null;
 		int input = 0;
 
-		//init MDP.state and MDP.action
+		/** init MDP.state and MDP.action */
 		for (int i = 0; i < State.length; i++) {
 			State[i] = i;
 			Action[i] = i;
@@ -45,11 +66,9 @@ public class MPDmain {
 		try {
 			reader = new BufferedReader(new FileReader(file));
 			while (reader.readLine() != null) {
-				//tempString  =reader.toString();
 				input = Integer.parseInt(reader.readLine());
 
 				int hashcodenum = input % PNUM;
-				//System.out.println("Hashcode = " + hashcodenum);
 
 				if (counter.containsKey(hashcodenum)) {
 					int tempvalue = counter.get(hashcodenum);
@@ -60,18 +79,31 @@ public class MPDmain {
 					Sampletable[0][hashcodenum] = hashcodenum;
 					Sampletable[1][hashcodenum] = 1;
 				}
-				//System.out.println("Counter = " + counter);
-				samplenum++;
 
-				//sample = 10%TOTALNUM, begin MDP
-				//				if (samplenum > TOTALNUM / 10) {
-				//					mdp();
-				//
-				//				}
+				Samplenum++;
 
+				/**
+				 * the moment to decide to mdp samplenum = 10%TOTALNUM, begin
+				 * MDP
+				 */
+				if ((Samplenum > Math.round((double) (TOTALNUM * 0.4))) && First) {
+					//System.out.println("Samplenum = " + Samplenum);
+					beta = Samplenum / TOTALNUM;
+					mdp();
+					First = false;
+				}else{
+					if(unAssignedPar.size() > 0){
+						mdp();
+					}
+					
+				}
+				
+				
+				
+				
 			}
 
-			mdp();
+			//mdp();
 
 			//按照hash值写入文件
 			String outstring = String.valueOf(counter);
@@ -104,22 +136,20 @@ public class MPDmain {
 
 	}
 
-	//MDP
+	/**
+	 * MDP
+	 */
 	public void mdp() {
 		int value[] = new int[PNUM];
+		double Evalue = 0.0;
 
-		//		for (int i = 0; i < Sampletable.length; i++) {
-		//		for (int j = 0; j < Sampletable[i].length; j++) {
-		//			System.out.println("Sampletable = " + Sampletable[i][j]);
-		//		}
-		//	}
-
+		/** sort samplingTable descending */
 		for (int i = 0; i < Sampletable[1].length; i++) {
 			int maxid = Sampletable[0][i];
 			int maxnum = Sampletable[1][i];
-			int maxdid  = 0 ;
+			int maxdid = 0;
 
-			for (int j = i+1 ; j < Sampletable[1].length; j++) {
+			for (int j = i + 1; j < Sampletable[1].length; j++) {
 				if (maxnum < Sampletable[1][j]) {
 					maxid = Sampletable[0][j];
 					maxnum = Sampletable[1][j];
@@ -133,20 +163,134 @@ public class MPDmain {
 				tempval = Sampletable[1][i];
 				Sampletable[0][maxdid] = Sampletable[0][i];
 				Sampletable[1][maxdid] = tempval;
-				
+
 				Sampletable[0][i] = maxid;
 				Sampletable[1][i] = maxnum;
 
-				
 			} else {
 				continue;
 			}
 		}
-
+		// output matrix[3][] 
 		for (int i = 0; i < Sampletable.length; i++) {
 			for (int j = 0; j < Sampletable[1].length; j++) {
-				System.out.println("Sampletable[" + i + "][" + j + "]=" + Sampletable[i][j]);
+				//System.out.println("Sampletable[" + i + "][" + j + "]=" + Sampletable[i][j]);
+				System.out.print(Sampletable[i][j]);
+				System.out.print(' ');
 			}
+			System.out.println(' ');
+		}
+
+		/** add assign & unassign paritition in a linkedlist */
+		for (int i = 0; i < Sampletable[2].length; i++) {
+			if (Sampletable[2][i] == 0) {
+				if (unAssignedPar.contains(Sampletable[0][i]) == false) {
+					unAssignedPar.add(Sampletable[0][i]);
+				}
+			} else {
+				if (Sampletable[2][i] == 1) {
+					if (AssignedPar.contains(Sampletable[0][i]) == false) {
+						AssignedPar.add(Sampletable[0][i]);
+					}
+				}
+			}
+		}
+		//System.out.println("unAssignedPar=" + unAssignedPar + "AssignedPar="+ AssignedPar);
+
+		/** caculate each Evalue */
+		double[] EvalueArray = new double[PNUM];
+		double problity = 0;
+
+		AssPnum = 0;
+		unAssPnum = 0;
+
+		UNAssiPNUM = unAssignedPar.size();
+
+		for (int i = 0; i < PNUM; i++) {
+			int unpnum = 0;
+			int asspnum = 0;
+			int unsqunum = 0;
+			int asqunum = 0;
+
+			if (i < UNAssiPNUM) {
+				unpnum = unAssignedPar.get(i);
+
+				for (int j = 0; j < PNUM; j++) {
+					if (j == unpnum) {
+						unsqunum = j;
+						break;
+					}
+				}
+				unAssPnum += Sampletable[1][unsqunum];
+			}
+
+			if (i < PNUM - UNAssiPNUM) {
+				asspnum = AssignedPar.get(i);
+
+				for (int j = 0; j < PNUM; j++) {
+					if (j == asspnum) {
+						asqunum = j;
+						break;
+					}
+				}
+				AssPnum += Sampletable[1][asqunum];
+			}
+
+			//PAsUna = (double) AssPnum / unAssPnum;
+			//System.out.println("AssPnum=" + AssPnum + "unAssPnum=" + unAssPnum + "Samplenum" + Samplenum);
+
+			problity = (double) unAssPnum / (TOTALNUM - Samplenum);
+			DecimalFormat df = new DecimalFormat("#.000000");
+			//System.out.println("Sampletable=" + df.format(problity));
+
+			EvalueArray[i] = problity * (TOTALNUM - Samplenum - unAssPnum);
+			//System.out.println("EvalueArray=" + EvalueArray[i]);
+		}
+
+		/** caculate the max value */
+		double maxVal = 0.0;
+		int maxAct = 0;
+		for (int i = 0; i < EvalueArray.length; i++) {
+			if (maxVal < EvalueArray[i]) {
+				maxVal = EvalueArray[i];
+				maxAct = i;
+			}
+		}
+		System.out.println("maxVal=" + maxVal + "maxAct=" + maxAct +"UNAssiPNUM="+UNAssiPNUM);
+
+		/** assign P in this round */
+		int getPround = -1;
+		int ungetPround = -1;
+		for (int i = 0; i < maxAct; i++) {
+			if (maxAct < UNAssiPNUM) {
+				ungetPround = unAssignedPar.get(0);
+
+				for (int j = 0; j < PNUM; j++) {
+					if (j == ungetPround) {
+						getPround = j;
+						break;
+					}
+				}
+
+				Sampletable[2][getPround] = 1;
+				unAssignedPar.remove(0);
+				AssignedPar.add(ungetPround);
+				
+				System.out.println("System.out.println"+unAssignedPar);
+			}
+
+			//AssignedPar.add
+		}
+		if(maxAct == 0){
+			for(int i=0; i<unAssignedPar.size();i++){
+				AssignedPar.add(unAssignedPar.get(0));
+				unAssignedPar.remove(0);
+				//Sampletable[2][getPround] = 1;
+			}
+		}
+
+		for (int i = 0; i < Sampletable[2].length; i++) {
+			System.out.println("Sampletable[2]" + Sampletable[2][i]);
 		}
 
 	}
@@ -155,7 +299,7 @@ public class MPDmain {
 		// TODO Auto-generated method stub
 
 		MPDmain mpd = new MPDmain();
-		mpd.readFileByLines("/home/wzhuo/example/mdp/test.txt");
+		mpd.readFileByLines("/home/wzhuo/example/mdp/zipf6.txt");
 
 	}
 
